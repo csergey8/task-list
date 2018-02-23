2b11c84e4fd29b9b33132854ff9649d19eff0705
csergey8 <c.sergey@protonmail.com>
2018-02-23 17:33:10 +0200

    added filter

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     // Clear task event
     clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
+
 }
 
 // Add task
@@ -62,4 +65,17 @@ function clearTasks(e) {
         taskList.removeChild(taskList.firstChild);
     }
     e.preventDefault();
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    })
 }
